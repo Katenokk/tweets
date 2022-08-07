653e18e0b4267d626aeb1ae1c2169be5bd2457c8
Katia Obraztsova <katia.obr@gmail.com>
2022-08-07 11:23:20 +0200

    hecho sin el bonus (meter inputs)

---
+++ src/components/Tweet.vue
@@ -0,0 +1,53 @@
+<template>
+  <div className="tweet">
+    <ProfileImage :image="user.image"/>
+
+
+    <div className="body">
+      <div className="top">
+        <User :userData="user"/>
+        <Timestamp :timestamp="timestamp"/>
+      </div>
+      <Message :message="message"/> 
+       <Actions/> 
+
+    </div> 
+
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
+</template>
+
+<script>
+import ProfileImage from './ProfileImage.vue'
+import User from "./User.vue"
+import Timestamp from './Timestamp.vue'
+import Message from './Message.vue'
+import Actions from "./Actions.vue"
+
+export default {
+  components: {
+    ProfileImage,
+    User,
+    Timestamp,
+    Message,
+    Actions
+  },
+  props: {
+    user: Object,
+    timestamp: String,
+    message: String,
+    
+
+  },
+  mounted() {
+    console.log(this.user)
+  }
+}
+</script>
+
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <span className="user">
+  <span className="name"> {{userData.name}} </span>
+  <span className="handle"> {{userData.handle}}</span>
+</span>
+</template>
+
+<script>
+export default {
+    props: {
+        userData: Object
+
+    }
+   
+
+}
+</script>
+
+<style>
+
+</style>
